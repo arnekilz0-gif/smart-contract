b2094c1fe86e8011335e5aafd7cc9e2c314a1a5b
arnekilz0-gif <arnekilz0@gmail.com>
2026-02-24 17:44:19 +0100

    Update parkingchanged.sol

--- parkingchanged.sol
+++ parkingchanged.sol
@@ -66,7 +66,7 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         oracle = _oracle; // oracle
         minDepositWei = _minDepositWei; // minimum deposit
     }
-    // Pause control
+    // ADMIN/ OWNER PAUSE/ UNPAUSE CONTROL
     // pause() and unpause() allow the owner to stop the mechanism. Designed to freeze flow without destroying state.
     // The following functions follow this code [https://docs.soliditylang.org/en/latest/contracts.html#functions]. Code from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility] and [https://docs.openzeppelin.com/contracts/5.x/api/access#Ownable] is used to change visibility and add access modifier.
     function pause() external onlyOwner { // Owner can stop actions that are marked with whenNotPaused
@@ -77,7 +77,7 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         _unpause(); // this code is taken from [https://docs.openzeppelin.com/contracts/5.x/api/utils#Pausable]
     }
 
-    // Admin setters
+    // ADMIN/ OWNER SETTINGS
     // The following functions have external visibility and an onlyOwner modifier with code from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility and https://docs.openzeppelin.com/contracts/5.x/api/access#Ownable] 
     function setOracle(address _oracle) external onlyOwner { // Changes the orcale key if key is compromised or change needed.
         require(_oracle != address(0), "zero oracle"); // Code is taken from here [https://docs.soliditylang.org/en/latest/contracts.html#custom-errors]. It now requires that the oracle address is not zero. Zero Oracle would permanently disable oracle restricted flows.
@@ -97,7 +97,7 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         checkInTimeoutSec = _timeoutSec;
     }
 
-    // Withdraw Logic
+    // WITHDRAW LOGIC
     // This is the only way to withdraw funds from the contract. Withdrawable shows how much balance is in the contract that is not a deposit of a driver that is still parking.
     // The withdraw function is then used to transfer the withdrawable balance to any address.
     // Shows how much the owner can safely withdraw (fees only).
@@ -122,7 +122,7 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         (bool ok, ) = to.call{value: amountWei}(""); // Send the amount to the specified address
         require(ok, "withdraw failed"); // Require that the transfer was successful
     }
-
+    // DRIVER FUNCTIONS
     // Driver check in 
     // The following functions with code from [https://docs.soliditylang.org/en/latest/contracts.html#functions] control the check in and the cancellation of a check in of a driver.
     // Visibility
@@ -170,32 +170,43 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         emit CheckInCancelled(spotId, driver, refundWei); // Emits CheckInCancelled
     }
 
+    // ORACLE FUNCTIONS
+    // The oracle can report occupied and report free and therefore determine how long the user has parked and then translate that into a fee that has to be paid.
     // Oracle reports occupied
-
+    // The subsequent code is taken from here [https://docs.soliditylang.org/en/latest/contracts.html#functions]. 
+    // External visibility and onlyOracle, whenNotPaused modifiers are added with code from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility; https://docs.openzeppelin.com/contracts/5.x/api/utils#Pausable]
     function reportOccupied(uint256 spotId) external onlyOracle whenNotPaused { // Pi confirms the arrival of user (car/ vehicle)
+        // According to solidity documentation [https://docs.soliditylang.org/en/latest/introduction-to-smart-contracts.html#locations] a variable declared with storage becomes a reference to a state variable. This line creates a reference on the saved spot, therefore all changes on s will be saved in the contract.
         Spot storage s = spots[spotId];
+        // Custom errors are added with code from [https://docs.soliditylang.org/en/latest/contracts.html#custom-errors]
         require(s.state == SpotState.CHECKED_IN, "not checked in"); // Require CHECKED_IN
-        require(block.timestamp <= s.checkInTime + checkInTimeoutSec, "check-in expired"); // Require that the check in has not expired
+        require(block.timestamp <= s.checkInTime + checkInTimeoutSec, "check-in expired"); // Code is taken from [https://docs.soliditylang.org/en/latest/units-and-global-variables.html#block-and-transaction-properties] and used to require that the check in has not expired
 
         s.state = SpotState.OCCUPIED; // Sets state to OCCUPIED
         s.startTime = block.timestamp; // Starts the time of parking
 
+        // With code from here [https://docs.soliditylang.org/en/latest/contracts.html#example] we emit the event Occupied and it is stored in the transaction logs.
         emit Occupied(spotId, s.startTime); // Emits Occupied
     }
 
     // Oracle report free
-
+    // External visibility, onlyOracle, nonReentrant and whenNotPaused are added with code taken from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility; https://docs.openzeppelin.com/contracts/5.x/api/utils#ReentrancyGuard; https://docs.openzeppelin.com/contracts/5.x/api/utils#Pausable].
     function reportFree(uint256 spotId) external onlyOracle nonReentrant whenNotPaused { // Pi confirms the car left, ends the billing of the user and resolves the contract
+        //  According to solidity documentation [https://docs.soliditylang.org/en/latest/introduction-to-smart-contracts.html#locations] a variable declared with storage becomes a reference to a state variable. This line creates a reference on the saved spot, therefore all changes on s will be saved in the contract.     
         Spot storage s = spots[spotId];
+        // Custom error is added with code from [https://docs.soliditylang.org/en/latest/contracts.html#custom-errors]
         require(s.state == SpotState.OCCUPIED, "not occupied"); // Requires OCCUPIED 
         _finalizeAndReset(spotId, s, block.timestamp); // Calls _finalizeAndReset
     }
 
-    // Owner emgerncy functions
+    // ADMIN/ OWNER EMERGENCY FUNCTIONS
 
     // Owner force reset spot
-
-    function forceResetSpot(uint256 spotId) external onlyOwner nonReentrant { // Allows owner for manual recovery when something is stuck or there are sensor issues. Function refunds the full deposit and resets the spot.
+    // Allows owner for manual recovery when something is stuck or there are sensor issues. Function refunds the full deposit and resets the spot.
+    // Both admin/ owner functions use external visibility, onlyOwner and nonReentrant modifiers with correspodning code from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility; https://docs.openzeppelin.com/contracts/5.x/api/access#Ownable; https://docs.openzeppelin.com/contracts/5.x/api/utils#ReentrancyGuard]
+    // Both functions use this concept: According to solidity documentation [https://docs.soliditylang.org/en/latest/introduction-to-smart-contracts.html#locations] a variable declared with storage becomes a reference to a state variable. This line creates a reference on the saved spot, therefore all changes on s will be saved in the contract.
+    // Custom error in both functions are added with code from [https://docs.soliditylang.org/en/latest/contracts.html#custom-errors]
+    function forceResetSpot(uint256 spotId) external onlyOwner nonReentrant { 
         Spot storage s = spots[spotId];
         SpotState oldState = s.state;
         require(oldState != SpotState.FREE, "already free"); // Require not FREE
@@ -207,14 +218,17 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         // Release reserved deposit before reset/refund.
         totalDepositsLocked -= refundWei;
 
-        _resetSpot(s); // Reset spot
+        // Reset spot
+        _resetSpot(s); 
 
         // Refund driver if present
-        if (refundWei > 0 && oldDriver != address(0)) {
-            (bool ok, ) = payable(oldDriver).call{value: refundWei}("");
+        if (refundWei > 0 && oldDriver != address(0)) { // Tests if refund is greater than zero and the address is not zero
+            // Converts oldDriver into a payable address and uses a call to send to send refundWei to the address. Ok saves whether the transfer was a success. 
+            (bool ok, ) = payable(oldDriver).call{value: refundWei}(""); // Code is taken from [https://docs.soliditylang.org/en/v0.8.34/cheatsheet.html#members-of-address]
             require(ok, "refund failed");
         }
 
+        // With code from here [https://docs.soliditylang.org/en/latest/contracts.html#example] we emit the event ForceReset and it is stored in the transaction logs.
         emit ForceReset(spotId, oldState, oldDriver, refundWei); // Emit ForceReset
     }
 
@@ -224,13 +238,17 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         Spot storage s = spots[spotId];
         require(s.state == SpotState.OCCUPIED, "not occupied"); // Requires OCCUPIED
 
-        // Calls _finalizeAndReset and emits ForceEnd
+        // Calls _finalizeAndReset 
         (uint256 feeWei, uint256 refundWei, address driver) = _finalizeAndReset(spotId, s, block.timestamp);
+        
+        // With code from here [https://docs.soliditylang.org/en/latest/contracts.html#example] we emit the event ForceEnd and it is stored in the transaction logs.
         emit ForceEnd(spotId, driver, feeWei, refundWei);
     }
 
-    // Internal Functions
-
+    // INTERNAL HELPER-FUNCTION
+    // Subsequent code is taken from [https://docs.soliditylang.org/en/latest/contracts.html#functions] to form the function and from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility] to set internal visibility. 
+    // It receives a storage reference and operates directly on persistent blockchain state
+    // Restores the spot to a clean and neutral state in persistent storage
     function _resetSpot(Spot storage s) internal {
         s.state = SpotState.FREE; // FREE
         s.driver = address(0); // zero address
@@ -239,14 +257,23 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         s.startTime = 0; // zero timestamps
     }
 
-    // Settlement helper (Core billing engine)
-
-    function _finalizeAndReset( // Bills the user and refunds the rest of deposit
-        uint256 spotId,
-        Spot storage s,
-        uint256 endTime
-    ) internal returns (uint256 feeWei, uint256 refundWei, address driver) {
-        require(endTime >= s.startTime, "time error"); // Require endTime >= startTime
+    // SETTLEMENT HELPER (CORE BILLING ENGINE)
+    // Bills the user and refunds the rest of deposit.
+    // Subsequent code is taken from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility] to model the function
+    // Function uses this concept: According to solidity documentation [https://docs.soliditylang.org/en/latest/introduction-to-smart-contracts.html#locations] a variable declared with storage becomes a reference to a state variable. This line creates a reference on the saved spot, therefore all changes on s will be saved in the contract.
+    function _finalizeAndReset( 
+        uint256 spotId, // parking spot
+        Spot storage s, // reference to stored struct
+        uint256 endTime // timestamp
+    
+    // Visibility is set to internal with code from [https://docs.soliditylang.org/en/latest/contracts.html#function-visibility]
+    // Subsequent code is taken from [https://docs.soliditylang.org/en/v0.8.34/contracts.html#return-variables]
+    ) internal returns (
+        uint256 feeWei, // parking fee
+        uint256 refundWei, // remaining deposit
+        address driver) { // driver address
+        // Custom error is added with code from [https://docs.soliditylang.org/en/latest/contracts.html#custom-errors]
+        require(endTime >= s.startTime, "time error"); // Require endTime greater or equal then startTime
 
         // Cache deposit
         uint256 deposit = s.depositWei;
@@ -276,17 +303,19 @@ contract ParkingPPX is Ownable, ReentrancyGuard, Pausable { // All three modules
         _resetSpot(s);
 
         // Refund driver if present
-        if (refundWei > 0 && driver != address(0)) {
-            (bool ok, ) = payable(driver).call{value: refundWei}("");
+        if (refundWei > 0 && driver != address(0)) { // Tests if refund is greater than zero and the address is not zero
+            // Converts driver into a payable address and uses a call to send to send refundWei to the address. Ok saves whether the transfer was a success. 
+            (bool ok, ) = payable(driver).call{value: refundWei}(""); // Code is taken from [https://docs.soliditylang.org/en/v0.8.34/cheatsheet.html#members-of-address]
             require(ok, "Refund failed");
         }
 
-        // Emit Freed
+        // With code from here [https://docs.soliditylang.org/en/latest/contracts.html#example] we emit the event Freed and it is stored in the transaction logs.
         emit Freed(spotId, endTime, minutesBilled, feeWei, refundWei, driver);
         
-        // Return fee/ refund
+        // Subsequent code is taken from [https://docs.soliditylang.org/en/v0.8.34/contracts.html#return-variables]
         return (feeWei, refundWei, driver);
     }
     // Allows direct Ether transfers
+    // Subsequent code is taken from [https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function]
     receive() external payable {}
 }
